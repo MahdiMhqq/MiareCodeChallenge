fb3af94f5eee42f4ef0ac363eccc7195d720e06e
Mahdi Mohaqeq <mahdi.mohaqeq.mm@gmail.com>
2023-01-12 12:34:39 +0330

    [ADD] Tailwind Configs + Folder Structure
    + VazirMatn google font link tags
    + Favicon
    - Remove unneccesary files and react boilerplates

---
+++ src/components/UI/Section/SubSection/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SubSection() {
+  return <div>SubSection</div>;
+}
+
+export default SubSection;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // Global
+        black: "#000",
+        white: "#fff",
+        gray: "#ECEDEF",
+        // Main
+        primary: "rgba(113,28,133,1)",
+        secondary: "rgba(248,191,0,1)",
+        // Text
+        tblack: "#4A4A4A",
+        tdanger: "#FA256A",
+        tsuccess: "#19C941",
+      },
+      fontFamily: {
+        vazir: ["Vazirmatn", "Serif"],
+      },
+    },
+  },
+  plugins: [],
+};
